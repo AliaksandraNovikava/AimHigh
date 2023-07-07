3579b69866bd955fc310051ca975779081309c8b
Aliaksandra Novikava <alexa_nov90@yahoo.de>
2023-07-07 10:55:16 +0200

    adjust styling

--- components/ProposedGoalPreview/index.js
+++ components/ProposedGoalPreview/index.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import styled from "styled-components";
+
+const StyledTitle = styled.p`
+  margin-bottom: 0;
+`;
 
 export default function ProposedGoalPreview({ image, title }) {
   return (
     <>
       <div>
-        <Image src={image} alt={title} width={48} height={48} />
-        <p>{title}</p>
+        <Image src={image} alt={title} width={40} height={40} />
+        <StyledTitle>{title}</StyledTitle>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import ProposedGoalsList from "@/components/ProposedGoalsList";
+import styled from "styled-components";
+
+const StyledHeading = styled.h2`
+  font-size: 21px;
+  font-weight: 500;
+  line-height: 1.5;
+`;
 
 export default function HomePage({ goals, categoryColors }) {
   return (
     <div>
-      <h1>Choose goal</h1>
+      <StyledHeading>Choose goal</StyledHeading>
       <ProposedGoalsList goals={goals} categoryColors={categoryColors} />
     </div>
   );
